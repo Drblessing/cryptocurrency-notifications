8e23d88c1fff480a6633d3344e7d4b5d4fc9d45d
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-03-31 23:11:06 -0700

    not working scraping

--- gcf/scraping/labelled_cex_addresses.py
+++ gcf/scraping/labelled_cex_addresses.py
@@ -5,31 +5,32 @@ from scrapy.crawler import CrawlerProcess,CrawlerRunner
 from twisted.internet import reactor
 from multiprocessing import Process, Queue
 from scrapy.utils.log import configure_logging
+from fake_useragent import UserAgent
 
-# Fix deprecation warnings
-import warnings
-from scrapy.exceptions import ScrapyDeprecationWarning
-warnings.filterwarnings("ignore", category=ScrapyDeprecationWarning)
+# # Fix deprecation warnings
+# import warnings
+# from scrapy.exceptions import ScrapyDeprecationWarning
+# warnings.filterwarnings("ignore", category=ScrapyDeprecationWarning)
 
 
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.propagate = False
-logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-# Create a console handler and set its level
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-# Create a formatter and set it on the console handler
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-# Add the console handler to the logger
-logger.addHandler(console_handler)
+# import logging
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# logger.propagate = False
+# logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# # Create a console handler and set its level
+# console_handler = logging.StreamHandler()
+# console_handler.setLevel(logging.INFO)
+# # Create a formatter and set it on the console handler
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# console_handler.setFormatter(formatter)
+# # Add the console handler to the logger
+# logger.addHandler(console_handler)
 
 class Gainers(scrapy.Spider):
     name = "gainers"
     start_urls = [
-        'https://www.coingecko.com/en/crypto-gainers-losers',
+        'https://etherscan.io/accounts/label/binance',
     ]
 
     def load_cookies_from_json(self,file_path):
@@ -41,24 +42,29 @@ class Gainers(scrapy.Spider):
         return cookies
 
     def start_requests(self):
-        
+        ua = UserAgent()
+        cookies = self.load_cookies_from_json('cookies.json')
 
+        for cookie_name,cookie_value in cookies.items():
+            # logger.info(f"Setting cookie {cookie_name} to {cookie_value}")
+            pass
+        
         for url in self.start_urls:
-            logger.info(f"Scraping {url}")
-            yield scrapy.Request(url=url, callback=self.parse)
+            # logger.info(f"Scraping {url}")
+            USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+            headers = {'User-Agent': USER_AGENT}
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse, cookies=cookies)
 
     def parse(self, response):
-        for row in response.xpath('(//table)[1]//tbody//tr'):
-            yield {
-                'name': row.xpath('.//td[3]//span[1]/text()').get().strip(),
-                'price': row.xpath('.//td[4]//span[1]/text()').get(),
-                'change': row.xpath('.//td[6]//span[1]/text()').get(),
-                'href_id': row.xpath('.//td[3]//a/@href').get().split('/')[-1],
-            }
+        # Save html
+        print('Saving html')
+        print(response)
+        with open('labelled_cex_addresses.html', 'w') as f:
+            f.write(response.text)
 
 def script(queue):
     try:
-        logging.getLogger('scrapy').propagate = False
+        # logging.getLogger('scrapy').propagate = False
         process = CrawlerProcess(settings={
             "FEEDS": {
                 "gainers.json": {"format": "json","overwrite": True,"mode":"w"}
@@ -80,7 +86,7 @@ def main():
     if result is not None:
         raise result
     
-    logger.info("Scraping complete.")
+    # logger.info("Scraping complete.")
     return "Scraping complete."
 
 if __name__=='__main__':
