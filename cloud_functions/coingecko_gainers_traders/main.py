ff23ae149f6084588fa670b12d01d6ab5df0c5b3
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-03 17:31:55 -0700

    cloud functions

--- cloud_functions/coingecko_gainers_traders/main.py
+++ cloud_functions/coingecko_gainers_traders/main.py
@@ -1,30 +1,38 @@
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
 # Create a console handler and set its level
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 # Create a formatter and set it on the console handler
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 
 # Add the console handler to the logger
 logger.addHandler(console_handler)
 
+
 def main():
     logger.info("Cloud function starting.")
     # 1. Scrape coingecko gainers.
     import scraping.scraper
+
     scraping.scraper.main()
 
     # 2. Get contract address for gainers that have them, with coingecko.
     import scraping.contract_addresses
+
     scraping.contract_addresses.main()
 
     # 3. Run SQL query on Google Big Query to find wallets that have receieved these tokens within the last 7 days.
+    import gbq.gbq
+
+    gbq.gbq.main()
+
 
 if __name__ == "__main__":
     main()
--- cloud_functions/coingecko_gainers_traders/scraping/scraper.py
+++ cloud_functions/coingecko_gainers_traders/scraping/scraper.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import scrapy
-from scrapy.crawler import CrawlerProcess,CrawlerRunner
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from twisted.internet import reactor
 from multiprocessing import Process, Queue
 import logging
@@ -8,50 +8,55 @@ import logging
 # Fix deprecation warnings
 import warnings
 from scrapy.exceptions import ScrapyDeprecationWarning
+
 warnings.filterwarnings("ignore", category=ScrapyDeprecationWarning)
 
 
 class Gainers(scrapy.Spider):
     name = "gainers"
     start_urls = [
-        'https://www.coingecko.com/en/crypto-gainers-losers',
+        "https://www.coingecko.com/en/crypto-gainers-losers",
+        "https://www.coingecko.com/en/crypto-gainers-losers?top=all",
     ]
-    def parse(self, response):
-        for row in response.xpath('(//table)[1]//tbody//tr'):
-            yield {
-                'name': row.xpath('.//td[3]//span[1]/text()').get().strip(),
-                'price': row.xpath('.//td[4]//span[1]/text()').get(),
-                'change': row.xpath('.//td[6]//span[1]/text()').get(),
-                'href_id': row.xpath('.//td[3]//a/@href').get().split('/')[-1],
-            }
 
+    def parse(self, response):
+        for row in response.xpath("(//table)[1]//tbody//tr"):
+            yield {
+                "name": row.xpath(".//td[3]//span[1]/text()").get().strip(),
+                "price": row.xpath(".//td[4]//span[1]/text()").get(),
+                "change": row.xpath(".//td[6]//span[1]/text()").get(),
+                "href_id": row.xpath(".//td[3]//a/@href").get().split("/")[-1],
+            }
 
 
 def script(queue):
     try:
-        logging.getLogger('scrapy').propagate = False
-        process = CrawlerProcess(settings={
-            "FEEDS": {
-                "gainers.json": {"format": "json","overwrite": True,"mode":"w"},
-            },
-        })
+        logging.getLogger("scrapy").propagate = False
+        process = CrawlerProcess(
+            settings={
+                "FEEDS": {
+                    "gainers.json": {"format": "json", "overwrite": True, "mode": "w"},
+                },
+            }
+        )
         process.crawl(Gainers)
         process.start()
         queue.put(None)
-    except Exception as e: 
+    except Exception as e:
         queue.put(e)
+
 
 def main():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
-    logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
     # Create a console handler and set its level
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
     # Create a formatter and set it on the console handler
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     console_handler.setFormatter(formatter)
 
     # Add the console handler to the logger
@@ -67,5 +72,6 @@ def main():
     logger.info("Scraping complete.")
     return "Scraping complete."
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
