d061c20a8f6300fbec6f836269dd21fc1745a642
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-03 17:56:51 -0700

    main

--- cloud_functions/coingecko_gainers_traders/main.py
+++ cloud_functions/coingecko_gainers_traders/main.py
@@ -1,22 +1,21 @@
-import logging
+def main(request):
+    import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-# Create a console handler and set its level
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+    # Create a console handler and set its level
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
 
-# Create a formatter and set it on the console handler
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-console_handler.setFormatter(formatter)
+    # Create a formatter and set it on the console handler
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console_handler.setFormatter(formatter)
 
-# Add the console handler to the logger
-logger.addHandler(console_handler)
+    # Add the console handler to the logger
+    logger.addHandler(console_handler)
 
-
-def main():
     logger.info("Cloud function starting.")
     # 1. Scrape coingecko gainers.
     import scraping.scraper
@@ -32,7 +31,22 @@ def main():
     import gbq.gbq
 
     gbq.gbq.main()
+    return f"Hello World!"
 
 
-if __name__ == "__main__":
-    main()
+# def hello_world(request):
+#     """Responds to any HTTP request.
+#     Args:
+#         request (flask.Request): HTTP request object.
+#     Returns:
+#         The response text or any set of values that can be turned into a
+#         Response object using
+#         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+#     """
+#     request_json = request.get_json()
+#     if request.args and "message" in request.args:
+#         return request.args.get("message")
+#     elif request_json and "message" in request_json:
+#         return request_json["message"]
+#     else:
+#         return f"Hello World!"
