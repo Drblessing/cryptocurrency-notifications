480bcbea0c907bb741bb91ce21b5d9e48bd9e6e8
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-05 16:28:42 -0700

    test external api

--- fe/pages/testSSR.tsx
+++ fe/pages/testSSR.tsx
@@ -1,12 +1,19 @@
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
+import { useEffect } from 'react';
 
 interface TestProps {
   test: string;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Test fetching
+  const testApi = 'https://randomuser.me/api/';
+  const res = await fetch(testApi);
+  console.log(process.env.R2_BUCKET_API_KEY);
+  const data = await res.json();
+  console.log(data.results[0].name.first);
   return {
-    props: { test: 'test' },
+    props: { test: data.results[0].name.first },
   };
 };
 
