adb26c82433b9acf85712829d71b73a066e084a0
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-05 14:23:01 -0700

    basic layout

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,6 +1,24 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { Container, Flex, Box } from '@chakra-ui/react';
 
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+      <Flex direction='column' minH='100vh'>
+        <Box as='header'>
+          <Navbar />
+        </Box>
+
+        <Box as='main' flex='1'>
+          hey
+        </Box>
+        <Box as='footer'>
+          <Footer />
+        </Box>
+      </Flex>
+    </div>
+  );
 }
