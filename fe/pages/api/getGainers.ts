6d33eeff82e56a05b8281bf008badb42c07c972c
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-10 13:28:00 -0400

    cloudflare stuff

---
+++ fe/pages/api/getGainers.ts
@@ -0,0 +1,24 @@
+import type { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'edge',
+};
+
+// Fix process type, declare it as any type
+declare const process: any;
+
+export default async function onRequest(context: any): Promise<any> {
+    
+
+
+  console.log(process.env.NODE_ENV);
+  try {
+    const obj = await process.env.CRYPTO_NOTIFICATIONS.get('gainers.csv');
+    if (obj === null) {
+      return new Response('Not found', { status: 404 });
+    }
+    return new Response(obj.body);
+  } catch (e: any) {
+    return new Response(e.message, { status: 500 });
+  }
+}
