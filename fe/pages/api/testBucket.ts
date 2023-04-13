2a931b98c6622a971083699208c27b90c35fee47
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-04-13 13:07:28 -0700

    test bucket

---
+++ fe/pages/api/testBucket.ts
@@ -0,0 +1,20 @@
+import type { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'edge',
+};
+
+// Fix process type, declare it as any type
+declare const process: any;
+
+export default async function onRequest(context: any): Promise<any> {
+  try {
+    const obj = await process.env.CRYPTO_NOTIFICATIONS.get('gainers.csv');
+    if (obj === null) {
+      return new Response('Not found', { status: 404 });
+    }
+    return new Response(obj.body);
+  } catch (e: any) {
+    return new Response(e.message);
+  }
+}
